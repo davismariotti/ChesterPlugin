767a5eee37b002a7660dd38c2c12db51524a4004
gomeow <davismariotti@gmail.com>
2013-06-08 21:07:44 -0400

    Update the updater

--- src/main/java/info/gomeow/chester/util/Updater.java
+++ src/main/java/info/gomeow/chester/util/Updater.java
@@ -1,50 +1,50 @@
 package info.gomeow.chester.util;
 
-import info.gomeow.chester.Chester;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.UnknownHostException;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-public class Updater extends Chester {
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
-    public boolean getUpdate(String v) throws IOException {
-        JSONObject json;
-        try {
-            json = getInfo();
-            String version = json.getString("dbo_version");
-            String link = json.getString("link");
-            Chester.LINK = link;
-            Chester.NEWVERSION = version;
-            if(!version.equalsIgnoreCase(v)) {
-                return true;
-            }
-        } catch(JSONException e) {
-            throw new IOException();
+public class Updater {
+
+    public Updater(String v) throws SAXException, IOException, ParserConfigurationException {
+        oldVersion = v.substring(0, 5);
+        HttpURLConnection connection = (HttpURLConnection) new URL("http://dev.bukkit.org/projects/chester/files.rss").openConnection();
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+        connection.setUseCaches(false);
+        Document feed = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(connection.getInputStream());
+        newVersion = feed.getElementsByTagName("title").item(1).getTextContent().substring(1);
+        String link = feed.getElementsByTagName("link").item(1).getTextContent();
+        this.link = new BufferedReader(new InputStreamReader(new URL("http://is.gd/create.php?format=simple&url=" + link).openStream())).readLine();
+        if(v.contains("SNAPSHOT") && !newVersion.equals(oldVersion)) {
+            update = false;
+            return;
         }
-        return false;
+        update = !newVersion.equals(oldVersion);
     }
 
-    public JSONObject getInfo() throws IOException {
-        URL url = new URL("http://api.bukget.org/3/plugins/bukkit/chester/latest");
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch(UnknownHostException e) {
-            throw new IOException();
-        }
-        JSONObject json;
-        try {
-            json = new JSONObject(in.readLine()).getJSONArray("versions").getJSONObject(0);
-            in.close();
-            return json;
-        } catch(JSONException e) {
-        }
-        return null;
+    private String oldVersion;
+    private String newVersion;
+    private String link;
+    private boolean update;
+
+    public boolean getUpdate() {
+        return update;
+    }
+
+    public String getNewVersion() {
+        return newVersion;
+    }
+
+    public String getLink() {
+        return link;
     }
 }
